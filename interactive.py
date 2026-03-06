304f5f6d1b9adc70a42bcb502ce9e36eb09a1ba0
simbon4ik <karpov.ivan123@mail.ru>
2026-03-06 19:25:44 +0300

    add interactive

---
+++ interactive.py
@@ -0,0 +1,11 @@
+from systemrdl import *
+
+try:
+    addr_input = input("Введите адрес в HEX (например, 0x42): ")
+    addr = int(addr_input, 16)
+    resp = reg_relation(addr, 0, "read")            
+    reg = Note(addr, resp["reg_value"], resp["status"], resp["ack"])
+    print(f"Регистр ADDR=0x{reg.addr} Значение {reg.reg_value} Статус {reg.status} Ак {reg.ack}\n")
+
+except Exception as e:
+    print(f"Error! addr={addr}, error={e}")
