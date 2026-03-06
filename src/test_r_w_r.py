df19268b1b30562fb99ebf5af20e9b0f81ae0efa
simbon4ik <karpov.ivan123@mail.ru>
2026-03-07 02:22:37 +0300

    change format output, find another bug with incorrect write

--- src/test_r_w_r.py
+++ src/test_r_w_r.py
@@ -58,14 +58,14 @@ def read_to_file(file_name, found_registers, errors):
         f.write("Registers without ack error\n")
         for reg in found_registers:        
             if reg.ack: 
-                f.write(f"ADDR=0x{reg.addr} VAL={reg.reg_value} ACK={reg.ack}\n")
+                f.write(f"ADDR=0x{reg.addr:04x} VAL={reg.reg_value:04x} ACK={reg.ack}\n")
         f.write("Registers with ack error\n")
         for reg in found_registers:
             if reg.ack == False: 
-                f.write(f"ADDR=0x{reg.addr} VAL={reg.reg_value} ACK={reg.ack}\n")
+                f.write(f"ADDR=0x{reg.addr:04x} VAL={reg.reg_value:04x} ACK={reg.ack}\n")
         f.write("Registers with exception\n")
         for addr, err in errors:
-            f.write(f"ADDR=0x{addr} ERROR={err}\n")  
+            f.write(f"ADDR=0x{addr:04x} ERROR={err}\n")  
         
     return found_registers, errors
 
