91be1ec714c2591319a3ba34b22e0de023ac32ff
bolshakov.mp <email@bk.ru>
2026-03-06 19:54:53 +0300

    prepare for mergeing

--- src/dut_interface.py
+++ src/dut_interface.py
@@ -3,15 +3,15 @@ from typing import Dict
 try:
     from riscv_reg_block import reg_access
 except ImportError:
-    def reg_access(addr: int, data: int, rw: str, bus_width: int = 32) -> Dict:
+    def reg_access(addr: int, data: int, rw: str) -> Dict:
         raise NotImplementedError(
             "riscv_reg_block.reg_access недоступен. "
         )
 
 
-def reg_read(addr: int, bus_width: int = 32) -> Dict:
-    return reg_access(addr=addr, data=0, rw="read", bus_width=bus_width)
+def reg_read(addr: int, data: int) -> Dict:
+    return reg_access(addr=addr, data=0, rw="read")
 
 
-def reg_write(addr: int, data: int, bus_width: int = 32) -> Dict:
-    return reg_access(addr=addr, data=data, rw="write", bus_width=bus_width)
+def reg_write(addr: int, data: int) -> Dict:
+    return reg_access(addr=addr, data=data, rw="write")
